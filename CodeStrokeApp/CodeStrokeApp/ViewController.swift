e1f09eef7f6fd8b97b28765e78ad288f3029c4e0
Moe <moe@Moes-MacBook-Pro.local>
2018-01-28 17:19:57 +1100

    project

---
+++ CodeStrokeApp/CodeStrokeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeStrokeApp
+//
+//  Created by Moe on 28/1/18.
+//  Copyright © 2018 Code Stroke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
